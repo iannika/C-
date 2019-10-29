8aa36e46e6024678f6aa5b7f4cecf12b369b1214
Hanna <55740693+iannika@users.noreply.github.com>
2019-10-29 13:34:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var c = new A(4, 83);
+            Console.WriteLine(c.sum_a());
+            Console.WriteLine(c.minus_a());
+            var d = new B(12, 0, 4);
+            Console.WriteLine(d.c2());
+        }
+    }
+}
+
+public class A {
+    private decimal a;
+    private decimal b;
+
+    public A (decimal a, decimal b) {
+        this.a = a;
+        this.b = b;
+    }
+    public decimal sum_a() {
+        return this.a+=this.b;
+    }
+    public decimal minus_a() {
+        return this.a - this.b;    
+    }
+
+}
+
+public class B:A {
+    private decimal d;
+    
+    public B (decimal a, decimal b, decimal d) : base(a, b){
+        this.d = d;
+    }
+
+    public decimal c2(){
+        while ((this.d) < 10){
+            return this.d++;
+        }
+    }
+
+}
